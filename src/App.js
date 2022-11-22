24e99d57c52aa428f5ba00ac6f9b4a0c68eac12f
David Lozada <davsh47@hotmail.com>
2022-11-23 02:29:32 +0300

    finished
    age calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Header from "./Header/Header";
+import "./App.css";
+import AgePicker from "./AgePicker/AgePicker";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AgePicker />
     </div>
   );
-}
+};
 
 export default App;
